7c62767728e8e96b482bf0ce3aa4b7ef95138cbf
mxmz-code <mc.simka+github@gmail.com>
2024-09-25 18:21:13 +0300

    record.py

---
+++ record.py
@@ -0,0 +1,29 @@
+# Модуль для зберігання записів контактів
+from field import Name, Phone
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        phone_obj = Phone(phone)
+        self.phones.append(phone_obj)
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        for i, phone in enumerate(self.phones):
+            if phone.value == old_phone:
+                self.phones[i] = Phone(new_phone)
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+
+    def __str__(self):
+        phones_str = ', '.join([str(phone) for phone in self.phones])
+        return f"Контакт: {self.name}, телефони: {phones_str}"
