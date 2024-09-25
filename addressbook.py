e118310294e6265304ca59331fd6a97e3ad9de9e
mxmz-code <mc.simka+github@gmail.com>
2024-09-25 18:04:23 +0300

    addressbook.py

---
+++ addressbook.py
@@ -0,0 +1,17 @@
+# Модуль для управління адресною книгою
+from collections import UserDict
+from record import Record
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name, None)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+    def __str__(self):
+        return '\n'.join(str(record) for record in self.data.values())
